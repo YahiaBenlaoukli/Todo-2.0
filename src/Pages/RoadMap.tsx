b39e27c40b9d7dda7f5073b7da86d6645043eceb
YahiaBenlaoukli <yahiabnlk@gmail.com>
2026-02-15 20:38:43 +0100

    raod map

--- src/Pages/RoadMap.tsx
+++ src/Pages/RoadMap.tsx
@@ -1,46 +1,53 @@
 import { useState, useCallback } from 'react';
-import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Background, Controls, Node, Edge, MiniMap } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import '../index.css'
 import Navbar from '../components/Navbar';
 
-const initialNodes = [
-    { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-    { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
-
 function RoadMap() {
-    const [nodes, setNodes] = useState(initialNodes);
-    const [edges, setEdges] = useState(initialEdges);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
+    const initialNodes = [
+        {
+            id: 'n1',
+            position: { x: 0, y: 0 },
+            data: { label: 'Node 1' },
+            type: 'input',
+        },
+        {
+            id: 'n2',
+            position: { x: 100, y: 100 },
+            data: { label: 'Node 2' },
+            type: 'output',
+        },
+    ];
+
+    const initialEdges = [
+        { id: 'n1-n2', source: 'n1', target: 'n2', animated: true, type: 'smoothstep', label: 'connects With' },
+    ];
+    const [nodes, setNodes] = useState<Node[]>(initialNodes)
+    const [edges, setEdges] = useState<Edge[]>(initialEdges)
 
     const onNodesChange = useCallback(
-        (changes: any) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-        [],
-    );
+        (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)), [])
     const onEdgesChange = useCallback(
-        (changes: any) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-        [],
-    );
-    const onConnect = useCallback(
+        (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)), [])
+    const connect = useCallback(
         (params: any) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
         [],
     );
 
+
+
+
     return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-                fitView
-            />
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <Navbar />
+            <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={connect} fitView panOnScroll={true} selectionOnDrag={true} panOnDrag={false}>
+                <Background />
+                <Controls />
+                <MiniMap />
+            </ReactFlow>
         </div>
     );
+
 }
 export default RoadMap;
